5412da9633db1b13b16310e1390b518cb833bdac
efg.river <efg.river@gmail.com>
2019-02-01 17:29:37 +0900

    add locations management page

--- lib/mylocal_cms_web/router.ex
+++ lib/mylocal_cms_web/router.ex
@@ -17,6 +17,7 @@ defmodule MylocalCmsWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/locations", LocationController, except: [:new, :edit]
   end
 
   # Other scopes may use custom stacks.
